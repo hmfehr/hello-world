32ffa557ce094abe154c7bbe2b992c02bdc8969c
Hunter Fehr <hunter.fehr3935@yahoo.com>
2022-09-02 11:47:04 -0700

    lab 6 update

--- app.js
+++ app.js
@@ -11,9 +11,32 @@ function greatUser(){
 //document.write('HI + userName');
 //console.log(userName)
 
+const myName = 'hunter'
+console.log(myName)
 
-
-function addTwoNumbers(num1, num2){
+function addTwoNumbers(){
+    let num1=prompt("put in a number")
+    let num2=prompt("put in a number")
     let totalnumber= num1 + num2  
     document.write(totalnumber)
 }
+
+
+let x = 10;
+console.log(x)
+
+function petOwner(){
+    let petOwner=prompt("Do you own a dog?")
+    if(petOwner== 'yes'){
+        document.write("Read list of bad food")
+    } 
+else if (petOwner== 'no'){
+    document.write("If considering getting a dog please read")
+    
+}
+else {
+    alert("error please answer yes or no")
+}
+}
+
+
