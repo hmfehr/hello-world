89274e72f18aeedc27b8d51393d9b95e29561871
Hunter Fehr <hunter.fehr3935@yahoo.com>
2022-09-01 17:36:28 -0700

    app update

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 'use strict';
 
 console.log('Hello World!');
+
+
+function greatUser(){
+    let userName =prompt ("what is your name");
+    document.write('HI ' + userName);
+}
+//let userName = greatUser
+//document.write('HI + userName');
+//console.log(userName)
+
+
+
+function addTwoNumbers(num1, num2){
+    let totalnumber= num1 + num2  
+    document.write(totalnumber)
+}
